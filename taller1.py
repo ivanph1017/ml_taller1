262b6071b835843f2240832233d8a742e7e51c82
Ivan Palomares <shades1017@gmail.com>
2018-09-13 04:40:11 -0500

    cross validation remains to do

--- taller1.py
+++ taller1.py
@@ -10,51 +10,99 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing, model_selection, linear_model
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-import pandas as pd
-from pandas.plotting import scatter_matrix
+import scipy.stats as st
 
 
-def load():
+def get_data():
     data = np.genfromtxt("dataR2.csv",delimiter=",")
-    data2=pd.read_csv("dataR2.csv",names=['Age', 'BMI','Glucose',
-                                          'Insulin', 'HOMA', 
-                                          'Leptin', 'Adiponectin'
-                                          'Resistin', 'MCP.1',
-                                          'Classification'])
-    # plot_corr(data2)
-    
-    X = np.concatenate((data[:,:-2], data[:,-1].reshape((-1, 1))), axis=1)
-    #X[X == '?'] = '-99999' #bad idea does not work
-    X[X == '?'] = 'NaN'
-    imputer = Imputer()
-    X = imputer.fit_transform(X)
+    X = data[1:,:-1]
+    means = np.mean(X, axis=0)
+    stds = np.std(X, axis=0)
+    # Formula para estandarizar
+    X = (X - means) / stds
     print(X)
-    y = data[:, -1].astype(int)
-    logisticR(X,y)
+    y = data[1:, -1].astype(int)
+    y = y - 1
+    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3)
+    return X_train, X_test, y_train, y_test
 
-def plot_corr(data2):
-    correlation=data2.corr()
-    plt.figure(figsize=(15, 10))
-    sns.heatmap(correlation, annot=True, cmap='coolwarm')
 
-def logisticR(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
+def logisticR(X_train, X_test, y_train, y_test):
     lg=linear_model.LogisticRegression(n_jobs = 10)
     lg.fit(X_train,y_train)
     predictions = lg.predict(X_test)
-    print(predictions)
+    print('Predicciones {}'.format(predictions))
     cm=confusion_matrix(y_test, predictions)
+    print('Matriz de confusion')
     print(cm)
-    score = lg.score(X_test, y_test)
-    print(score)
-    print("Accuracy: %0.2f (+/- %0.2f)" % (score.mean(), score.std() * 2))
     print('Accuracy de Sklearn {}'.format(accuracy_score(y_test, predictions)))
+    stds = np.std(X_train, axis=0)
+    return lg.intercept_[0], lg.coef_[0], stds, predictions
+    
+    
+def z_test(coef, std):
+    #z score
+    z = coef / std
+    # p-value
+    p = st.norm.cdf(z)
+    print('Z score: {}'.format(z))
+    print('P value: {:0.8f}'.format(p))
+    return z, p
+
+def g_test(y_test, predictions):
+    actual = y_test + 1
+    estimates = predictions + 1
+    y_ith = estimates * np.log(estimates / actual)
+    #g score
+    g = 2 * np.sum(y_ith)
+    # n - 1 degrees of freedom
+    df = y_test.shape[0] - 1
+    # p-value
+    p = st.chi2.cdf(g, df)
+    print('G score: {}'.format(g))
+    print('P value: {:0.8f}'.format(p))
+    return g, p
       
 def main():
-    load()
+    X_names = ['Age', 'BMI','Glucose', 'Insulin', 
+             'HOMA', 'Leptin', 'Adiponectin', 
+             'Resistin', 'MCP.1']
+    print('Primera corrida\n\n')
+    X_train, X_test, y_train, y_test = get_data()
+    intercept, coefs, stds, predictions = logisticR(X_train, X_test, y_train, y_test)
+    print('Intercepto {}'.format(intercept))
+    print('Coeficientes {}'.format(coefs))
+    cols_discarded = []
+    for j in range(X_train.shape[1]):
+        print('\nFeature {}'.format(X_names[j]))
+        z, p = z_test(coefs[j], stds[j])
+        # Si el intervalo de confianza es estricto al 95%, se queda sin features
+        # if p >= 0.025 and p <= 0.975
+        if p >= 0.25 and p <= 0.75:
+            print('Feature to discard'.format(X_names[j]))
+            cols_discarded.append(j)
+    print('\nModelo 1')
+    g, p_g = g_test(y_test, predictions)
+    
+    # X = np.concatenate((data[:,:-2], data[:,-1].reshape((-1, 1))), axis=1)
+    cols = list(filter(lambda i: not i in cols_discarded, range(len(X_names))))
+    X_names_remaining = list(map(lambda i: X_names[i], cols))
+    X_train = np.delete(X_train, cols_discarded, 1)
+    X_test =  np.delete(X_test, cols_discarded, 1)
 
+    print('\n\n2da corrida')
+    intercept, coefs, stds, predictions = logisticR(X_train, X_test, y_train, y_test)
+    print('Intercepto {}'.format(intercept))
+    print('Coeficientes {}'.format(coefs))
+    for j in range(X_train.shape[1]):
+        print('\nFeature {}'.format(X_names_remaining[j]))
+        z, p = z_test(coefs[j], stds[j])
+    print('\nModelo 2')
+    g, p_g = g_test(y_test, predictions)
+    print('\n\n')
+    
 if __name__=="__main__":
     main()
